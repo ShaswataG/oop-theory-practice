61f0f5038985698bd59a96c08280944b18913958
ShaswataG <121812904+ShaswataG@users.noreply.github.com>
2023-12-11 00:48:24 +0530

    other college papers

--- AEC CAT-2/1.a.Template.cpp
+++ AEC CAT-2/1.a.Template.cpp
@@ -1,6 +1,13 @@
 #include<iostream>
 using namespace std;
 
-int main() {
+template<class T, class U, class V>
+void display(T a, U b, V c) {
+    cout << "Display(" << a<< ", " << b << ", " << c << ")" << endl;
+}
 
+int main() {
+    display("AEC_CSE", 781013, 85.65);
+    display(100, "AEC_Tech", 90.5); 
+    return 0;
 };
--- AEC CAT-2/1.b.Minimum.cpp
+++ AEC CAT-2/1.b.Minimum.cpp
@@ -1,11 +1,21 @@
 #include<iostream>
 using namespace std;
 
-template<class T, class U, class V>
-T minimum(T a, U b, V c) {
+template<class T>
+T minimum(T a, T b, T c) {
     T min = a;
+    if (b < min) {
+        min = b;
+    }
+    if (c < min) {
+        min = c;
+    }
+    return min;
 }
 
 int main() {
+    cout << minimum(6, 2, 3) << endl;
+    cout << minimum(4.3, 7.5, 0.3) << endl;
+    cout << minimum('g','d','z') << endl;
     return 0;
 }
